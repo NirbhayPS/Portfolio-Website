4aaf1f56cbd17d470de22092271a56ea2348cecf
NirbhayPS <ms7738649@gmail.com>
2023-06-28 09:24:11 +0530

    added more content

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -20,7 +20,7 @@ const Sidenav = () => {
                         </a>
                         <a onClick={handleNav} href="#work" className='w-[75%] flex justify-center items-center rounded-full  shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                             <GrProjects size={20} />
-                            <span className='pl-4'>Work</span>
+                            <span className='pl-4'>Education</span>
                         </a>
                         <a onClick={handleNav} href="#projects" className='w-[75%] flex justify-center items-center rounded-full  shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                             <AiOutlineProject size={20} />
