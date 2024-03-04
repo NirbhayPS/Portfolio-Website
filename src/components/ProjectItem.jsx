72b5045b10a04efcfa7fd39b261c928b6baa96e6
Nirbhay Pratap Singh <155529583+nirbhay-bw@users.noreply.github.com>
2024-03-05 00:37:01 +0530

    done:qa \

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -8,7 +8,7 @@ const ProjectItem = ({img, title, link}) => {
             <h3 className="text-2xl font-bold text-white tracking-wider text-center">
                 {title}
             </h3>
-            <p className="pb-4 pt-2 text-gray-700 text-center">React Js</p>
+            <p className="pb-4 pt-2 text-gray-700 text-center">Reactjs</p>
             <a href={link}>
                 <p className="text-center p-3 rounded-id bg-white text-gray-700 font-bold cursor-pointer text-lg">More info</p>
             </a>
